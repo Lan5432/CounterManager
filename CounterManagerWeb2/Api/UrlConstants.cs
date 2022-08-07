3f864a87fe4b06dda9d8371be6fbc9589d97921e
Lan5432 <Lan5432@hotmail.com>
2022-08-07 12:22:50 +0200

    Removed test Web2 project.

--- CounterManagerWeb2/Api/UrlConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CounterManagerWeb2.Api {
-    public static class UrlConstants {
-
-        private const string BASE_URL = "api/";
-
-        public const string CounterApiUrl = BASE_URL + "Counter";
-        public static Func<long, string> CounterApiIdUrl = id => $"{CounterApiUrl}/{id}";
-    }
-}
--- CounterManagerWeb2/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace CounterManagerWeb.Models {
-    public class dsadsadas {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
